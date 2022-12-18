9eaba085c567595767bd5243284faa00bbbf33c0
Anthony Das <dev@anthnydas.com>
2022-12-18 02:01:26 -0500

    placeholder text

--- src/bot.py
+++ src/bot.py
@@ -5,10 +5,10 @@ def run(token):
 
     @bot.event
     async def on_ready():
-        print(f"{bot.user} is ready and online!")
+        print(f"{bot.user} connected!")
 
-    @bot.slash_command(name = "hello", description = "Say hello to the bot")
+    @bot.slash_command(name = "lyric", description = "grab a random lyric from a song")
     async def hello(ctx):
-        await ctx.respond("Hey!")
+        await ctx.respond("> Lyric goes here")
 
     bot.run(token)
